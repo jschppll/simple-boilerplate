0800cfa1edbb3a09c23c09d7c021e3d1385eeba8
Jessica <jessica@Jessicas-Air.home>
2021-07-07 13:34:47 -0700

    Add path aliases

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import "./styles/styles.scss";
+import "styles/styles.scss";
 
 axios("http://localhost:3001/test", {}).then(({ data }) => {
     document.body.innerHTML = data;
